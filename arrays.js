db8eb90271d665a71d72850f35d39c907a00cc41
Syed Imaduddin <informsyedimad@gmail.com>
2023-10-20 09:23:36 +0530

    Arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+//ARRAYS
+
+var marks = [50, 60, 70, 80, "syed"];
+console.log(Array.isArray(marks));
+marks.push("web");
+marks.pop();
+marks.shift();
+marks.unshift("imad")
+console.log(marks.length);
+console.log(marks.join());
+console.log(marks.reverse());
